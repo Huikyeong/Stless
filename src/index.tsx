ac1edd142badf57560ab2054abc9efc53c25549e
Huikyeong <dksrud0907@kaist.ac.kr>
2023-05-03 22:58:25 +0900

    feat: global font style 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/App';
+import Analysis from './pages/Analysis';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Global
+      styles={css`
+        * {
+          font-family: 'Noto Sans' !important;
+          user-select: none;
+        }
+      `}
+    />
+    <Analysis />
   </React.StrictMode>,
 );
