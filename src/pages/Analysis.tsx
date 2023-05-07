bd9c708afc100a9d988cf0bbb213ce7325f7b77e
Huikyeong <dksrud0907@kaist.ac.kr>
2023-05-07 23:29:45 +0900

    refactor: analysis layout 변경

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -2,6 +2,7 @@
 import { css } from '@emotion/react';
 import Calendar from 'components/Calendar';
 import Header from 'components/Header';
+import Plot from 'react-plotly.js';
 
 function Analysis() {
   return (
@@ -31,8 +32,96 @@ function Analysis() {
           box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
         `}
       >
-        Analysis!
-        <Calendar />
+        <div
+          css={css`
+            box-sizing: border-box;
+            display: flex;
+            align-items: center;
+            gap: 20px;
+
+            width: 1040px;
+            height: 100%;
+
+            padding-top: 30px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              align-items: flex-end;
+              gap: 50px;
+              height: 100%;
+            `}
+          >
+            <Calendar />
+            <div
+              css={css`
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+              `}
+            >
+              <Plot
+                data={[
+                  {
+                    y: [
+                      'giraffes',
+                      'orangutans',
+                      'monkeys',
+                      'huikyeong',
+                      'cheolhwan',
+                    ],
+                    x: [5, 14, 16, 20, 23],
+                    type: 'bar',
+                    orientation: 'h',
+                  },
+                ]}
+                layout={{
+                  width: 350,
+                  height: 150,
+                  margin: {
+                    l: 100,
+                    r: 0,
+                    b: 40,
+                    t: 0,
+                    pad: 4,
+                  },
+                  colorway: ['#E26464'],
+                }}
+              />
+              <Plot
+                data={[
+                  {
+                    y: [
+                      'giraffes',
+                      'orangutans',
+                      'monkeys',
+                      'huikyeong',
+                      'cheolhwan',
+                    ],
+                    x: [5, 14, 16, 20, 23],
+                    type: 'bar',
+                    orientation: 'h',
+                  },
+                ]}
+                layout={{
+                  width: 350,
+                  height: 150,
+                  margin: {
+                    l: 100,
+                    r: 0,
+                    b: 40,
+                    t: 0,
+                    pad: 4,
+                  },
+                  colorway: ['#6496E2'],
+                }}
+              />
+            </div>
+          </div>
+          <div>line chart</div>
+        </div>
       </div>
     </div>
   );
