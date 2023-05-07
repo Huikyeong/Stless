90e18e8cace627d64518bef5859e0356610c9552
Huikyeong <dksrud0907@kaist.ac.kr>
2023-05-07 20:06:11 +0900

    feat: hitmap 컬러 매칭 로직 추가

--- src/components/CalendarItem.tsx
+++ src/components/CalendarItem.tsx
@@ -1,11 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { colors } from 'utils/style';
 
 function CalendarItem(props: {
   num: number | undefined;
   val: number | undefined;
 }) {
   // value에 따라서 color 매칭
+  let backColor;
+  if (props.val === undefined) {
+    backColor = 'white';
+  } else if (props.val > 1.5) {
+    backColor = colors.hitmap1;
+  } else if (props.val > 0.5) {
+    backColor = colors.hitmap2;
+  } else if (props.val > -0.5) {
+    backColor = colors.hitmap3;
+  } else if (props.val > -1.5) {
+    backColor = colors.hitmap4;
+  } else {
+    backColor = colors.hitmap5;
+  }
+
   return (
     <div
       css={css`
@@ -29,9 +45,9 @@ function CalendarItem(props: {
             font-weight: 200;
             font-size: 18px;
             letter-spacing: -0.1em;
+            background-color: ${backColor};
             ${props.val
               ? `color: white;
-            background: #a92222;
             cursor: pointer;`
               : `color: #b4b4b4;`}
           `}
