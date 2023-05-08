d9bbf8cefa95be5c595c2ffd384a4d55986ae94f
Dongwon Choi <cookie000215@gmail.com>
2023-05-09 01:53:29 +0900

    Feat: read json in bar graphs

--- src/components/BarGraph.tsx
+++ src/components/BarGraph.tsx
@@ -1,9 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import Plot from 'react-plotly.js';
+import stressCauses from '../assets/datas/stress_cause.json';
+import stressSolutions from '../assets/datas/stress_solution.json';
 
 function BarGraph() {
   /* eslint-disable */
+  const compare = (
+    a: { category: String; value: number },
+    b: { category: String; value: number },
+  ) => a.value - b.value;
+
+  const yCauses = stressCauses.sort(compare).map((d) => d.category);
+  const xCauses = stressCauses.sort(compare).map((d) => d.value);
+  const ySolution = stressSolutions.sort(compare).map((d) => d.category);
+  const xSolution = stressSolutions.sort(compare).map((d) => d.value);
 
   return (
     <div
@@ -16,8 +27,9 @@ function BarGraph() {
       <Plot
         data={[
           {
-            y: ['giraffes', 'orangutans', 'monkeys', 'huikyeong', 'cheolhwan'],
-            x: [5, 14, 16, 20, 23],
+            y: yCauses,
+            x: xCauses,
+            hoverinfo: 'skip',
             type: 'bar',
             orientation: 'h',
           },
@@ -31,6 +43,9 @@ function BarGraph() {
             b: 40,
             t: 0,
             pad: 4,
+          },
+          xaxis: {
+            visible: false,
           },
           colorway: ['#E26464'],
         }}
@@ -38,8 +53,9 @@ function BarGraph() {
       <Plot
         data={[
           {
-            y: ['giraffes', 'orangutans', 'monkeys', 'huikyeong', 'cheolhwan'],
-            x: [5, 14, 16, 20, 23],
+            y: ySolution,
+            x: xSolution,
+            hoverinfo: 'skip',
             type: 'bar',
             orientation: 'h',
           },
@@ -53,6 +69,9 @@ function BarGraph() {
             b: 40,
             t: 0,
             pad: 4,
+          },
+          xaxis: {
+            visible: false,
           },
           colorway: ['#6496E2'],
         }}
