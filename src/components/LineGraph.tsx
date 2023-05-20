d1032ae80f488adad17b88b36ed56ce8b2c2cba1
kadiace <rlacjfghks95@gmail.com>
2023-05-20 14:25:05 +0900

    Success highlight.

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import * as dfd from 'danfojs';
+import { Shape } from 'plotly.js';
 import { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import { useRecoilValue } from 'recoil';
@@ -13,12 +14,8 @@ function LineGraph() {
   const [dfStressQuery, setDfStressQuery] = useState<dfd.DataFrame>(
     new dfd.DataFrame(),
   );
-  const [dfActivity, setDfActivity] = useState<dfd.DataFrame>(
-    new dfd.DataFrame(),
-  );
-  const [dfActivityQuery, setDfActivityQuery] = useState<dfd.DataFrame>(
-    new dfd.DataFrame(),
-  );
+  const [dfActivity, setDfActivity] = useState<Partial<Shape>[]>([]);
+  const [dfActivityQuery, setDfActivityQuery] = useState<Partial<Shape>[]>([]);
   const [dateRange, setDateRange] = useState<[number, number]>([
     selectedRange.start ?? 7,
     selectedRange.end ?? 14,
@@ -36,13 +33,34 @@ function LineGraph() {
       .catch((err) => console.log(err));
   }
 
-  if (dfActivityQuery.size == 0) {
+  if (dfActivityQuery.length == 0) {
     const rawDetected = require('assets/datas/detected_p0703.csv');
     dfd
       .readCSV(rawDetected)
       .then((df: dfd.DataFrame) => {
-        setDfActivity(df);
-        setDfActivityQuery(df);
+        const shapes: Partial<Shape>[] = [];
+        for (let i = 0; i < df.shape[0]; i++) {
+          const shape: Partial<Shape> = {
+            type: 'rect',
+            layer: 'below',
+            y0: -3.5,
+            y1: 3.5,
+            fillcolor:
+              df.iloc({ rows: [i] })['type'].values[0] == 'release'
+                ? '#6496E2'
+                : '#E26464',
+            opacity: 0.4,
+            line: {
+              color: 'rgba(0,0,0,0)',
+            },
+          };
+          shape.name = df.iloc({ rows: [i] })['activity'].values[0];
+          shape.x0 = df.iloc({ rows: [i] })['start'].values[0];
+          shape.x1 = df.iloc({ rows: [i] })['end'].values[0];
+          shapes.push(shape);
+        }
+        setDfActivity(shapes);
+        setDfActivityQuery(shapes);
       })
       .catch((err) => console.log(err));
   }
@@ -50,11 +68,10 @@ function LineGraph() {
   useEffect(() => {
     if (
       dfStressQuery.size > 0 &&
-      dfActivityQuery.size > 0 &&
+      dfActivityQuery.length > 0 &&
       selectedRange.start &&
       selectedRange.end
     ) {
-      console.log(dfStress['date'].values);
       setDfStressQuery(
         dfStress
           .query(
@@ -65,10 +82,10 @@ function LineGraph() {
           .resetIndex(),
       );
       setDfActivityQuery(
-        dfActivity.query(
-          dfActivity['start']
-            .gt(selectedRange.start - 1)
-            .and(dfActivity['end'].lt(selectedRange.end + 1)),
+        dfActivity.filter(
+          (shape) =>
+            new Date(shape.x0!).getDate() > selectedRange.start! - 1 &&
+            new Date(shape.x1!).getDate() < selectedRange.end! + 1,
         ),
       );
       setDateRange([selectedRange.start, selectedRange.end]);
@@ -142,22 +159,7 @@ function LineGraph() {
               zeroline: false,
               range: [-3.5, 3.5],
             },
-            shapes: [
-              {
-                type: 'rect',
-                name: 'exercise',
-                layer: 'below',
-                x0: '2019-05-10 16:37:17',
-                x1: '2019-05-12 13:38:36',
-                y0: -3.5,
-                y1: 3.5,
-                fillcolor: '#6496E2',
-                opacity: 0.4,
-                line: {
-                  color: 'rgba(0,0,0,0)',
-                },
-              },
-            ],
+            shapes: dfActivityQuery,
           }}
         />
       </div>
